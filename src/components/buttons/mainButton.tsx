cb42a5a688cff2f8f55306f5a23e1e8590242b46
Sebastián Lozano <sebastianl2.dev@gmail.com>
2025-10-14 23:13:11 -0500

    feat: Create button component

---
+++ src/components/buttons/mainButton.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+export default function MainButton({
+  children,
+  href,
+  primary,
+}: {
+  children: React.ReactNode;
+  href: string;
+  primary?: boolean;
+}) {
+  return (
+    <>
+      {primary ? (
+        <Link
+          className="group bg-amber-300 px-5 py-3.5 font-normal rounded-2xl hover:scale-105 button hover:bg-[#f8cc2e] text-neutral-950 shadow-yellow-center/70 flex gap-1 items-center"
+          href={href}
+        >
+          {children}
+        </Link>
+      ) : (
+        <Link
+          className="bg-neutral-200 px-5 py-3.5 font-normal rounded-2xl hover:scale-105 button hover:bg-neutral-300/75 shadow-black-center/15 flex gap-1 items-center"
+          href={href}
+        >
+          {children}
+        </Link>
+      )}
+    </>
+  );
+}
